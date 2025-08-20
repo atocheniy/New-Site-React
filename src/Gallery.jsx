ea6a02edd73f21f88a2106e081b6df6dd86858a8
Andrey Tochony <atochony@gmail.com>
2025-08-20 15:08:14 +0300

    commit

--- src/Gallery.jsx
+++ src/Gallery.jsx
@@ -6,6 +6,9 @@ import Card from "./components/card";
 
 import ExitToAppIcon from '@mui/icons-material/ExitToApp';
 
+import Icon1 from './assets/img3.png';
+import Icon2 from './assets/2.png';
+
 import './Gallery.css'
 
 function Gallery() {
@@ -17,14 +20,14 @@ function Gallery() {
                 <label style={{position: "absolute", zIndex: "2", paddingLeft: "15px"}}>Code Editor</label>
                 <div style={{borderRadius: "20px", width: "375px", height: "80px", background: "rgba(19, 19, 19, 0.7)", filter: "blur(40px)", backdropFilter: "blur(10px)", position: "absolute", zIndex: "1", paddingLeft: "15px", marginTop: "-30px"}}></div>
                 <button className="button" style={{position: "absolute", zIndex: "2", width: "60px", height: "60px", right: "15px", bottom: "15px", borderRadius: "30%"}}><ExitToAppIcon /></button>
-                <img src="./public/img3.png" alt="Image" style={{border: "1px solid red", boxShadow: "0 0 200px rgba(255, 0, 0, 0.2)"}}></img>
+                <img src={Icon1} alt="Image" style={{border: "1px solid red", boxShadow: "0 0 200px rgba(255, 0, 0, 0.2)"}}></img>
             </Card>
 
             <Card>
                 <label style={{position: "absolute", zIndex: "2", paddingLeft: "15px"}}>Tetris</label>
                 <div style={{borderRadius: "20px", width: "375px", height: "80px", background: "rgba(19, 19, 19, 0.7)", filter: "blur(40px)", backdropFilter: "blur(10px)", position: "absolute", zIndex: "1", paddingLeft: "15px", marginTop: "-30px"}}></div>
                 <button className="button" style={{position: "absolute", zIndex: "2", width: "60px", height: "60px", right: "15px", bottom: "15px", borderRadius: "30%"}}><ExitToAppIcon /></button>
-                <img src="./public/2.png" alt="Image" style={{border: "1px solid blue", boxShadow: "0 0 200px rgba(0, 68, 255, 0.2)"}}></img>
+                <img src={Icon2} alt="Image" style={{border: "1px solid blue", boxShadow: "0 0 200px rgba(0, 68, 255, 0.2)"}}></img>
             </Card>
 
             <Card name="Phone Book" >
